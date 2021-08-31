46d1211df5a1cc07d73bfdec1c1a82c7e457bd6d
vin <pythont2021@gmail.com>
2021-09-01 00:19:33 +0530

    todolist complited

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const request = require("request");
 const bodyParser= require("body-parser");
 const ejs = require('ejs');
 
+
 /////////////////////////////// CONFIGURE APP /////////////////////
 
 const app = express()
@@ -17,6 +18,11 @@ const port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
+/////////////////////////////// CONSTANTS /////////////////////
+
+var homeItems = [];
+var workItems =[]
 
 /////////////////////////////// GET ROUTE /////////////////////
 
@@ -29,10 +35,46 @@ app.get("/",(req, res)=>{
   }
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list.ejs", {thisDay:day});
+  res.render("list.ejs", {listTitle:day, newItems:homeItems});
 });
 
+
+app.get("/work", (req, res)=>{
+
+  res.render("list.ejs", {listTitle:"work list", newItems:workItems});
+});
+
+app.get("/about", (req, res)=>{
+
+  res.render("about");
+});
+
+app.get("/contact", (req, res)=>{
+
+  res.render("contact");
+});
+
+
 /////////////////////////////// POST ROUTE /////////////////////
+
+app.post("/", (req, res)=>{
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "work"){
+    workItems.push(item)
+    res.redirect("/work");
+  }else{
+    homeItems.push(item);
+    console.log(req.body.list);
+    res.redirect("/");
+  }
+
+
+});
+
+
+
 
 
 
